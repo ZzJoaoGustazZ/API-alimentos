354c768347d5ff9376d7c1c10f8e71030a538f7b
ZzJoaoGustazZ <joaogustavounifor@gmail.com>
2025-05-22 19:47:04 -0300

    Nova dependência, correção ao abrir o servidor.

--- api/project.clj
+++ api/project.clj
@@ -1,11 +1,16 @@
 (defproject api "0.1.0-SNAPSHOT"
             :description "API em clojure"
-            :dependencies [[org.clojure/clojure "1.10.3"]
+            :dependencies [
+
+                           [org.clojure/tools.logging "1.3.0"]
+                           [org.clojure/clojure "1.10.3"]
                            [metosin/compojure-api "2.0.0-alpha30"]
                            [ring/ring-core "1.9.3"]
                            [ring/ring-jetty-adapter "1.9.3"]
                            [clj-http "3.12.3"]
-                           [cheshire "5.10.0"]]
+                           [cheshire "5.10.0"]
+
+                           ]
 
 
             :main api.server
